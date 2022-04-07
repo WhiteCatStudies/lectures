d3f7b58aa34f9417df3c3187f75aebe80ea6dcf1
ankapoganka <kocherovaAA@yandex.ru>
2022-04-07 01:41:19 -0700

    Lecture3 examples: build directories deleted

--- pt1/lectures/lecture3/examples/2_hello_button/build/CMakeFiles/hellobutton_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/hellobutton_autogen"
-  "hellobutton_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/hellobutton_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
